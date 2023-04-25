409684ee81f18bf6447c3d13c26ebe70f0bb7ede
TerryL01234 <t.lambova23@acsbg.org>
2023-04-25 10:29:41 +0300

    maybe ready with c# and sql

--- LambovaT_TeaApp/Login.xaml.cs
+++ LambovaT_TeaApp/Login.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -15,17 +17,48 @@ namespace LambovaT_TeaApp
     /// <summary>
     /// Interaction logic for Window1.xaml
     /// </summary>
-    public partial class Login: Window
+    public partial class Login : Window
     {
+        public string PassTable { get; private set; }
+
         public Login()
         {
             InitializeComponent();
         }
         private void Confirm(object sender, RoutedEventArgs e)
         {
-            TeaCatalogue mwC = new TeaCatalogue();
-            mwC.Show();
-            this.Close();
+            SqlConnection sqlCon = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=H:\2022_2023\Informatics\Applications\FinalTeaApp\LambovaT_TeaApp\LambovaT_TeaApp\TeaDB.mdf;Integrated Security=True");
+            try
+            {
+                sqlCon.Open();
+                string query = "SELECT Pass FROM Users WHERE Email='this.Email'";
+                SqlCommand cmd = new SqlCommand(query, sqlCon);
+                object result = cmd.ExecuteScalar();
+                string passTable = (result == null ? "" : result.ToString());
+                cmd.ExecuteNonQuery();
+
+                if (!(GetPassword() == passTable))
+                {
+                    MessageBox.Show("Passwords don't match.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                else
+                {
+                    MessageBox.Show("Welcome back!");
+
+                    TeaCatalogue mwC = new TeaCatalogue();
+                    mwC.Show();
+                    this.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            string GetPassword()
+            {
+                return Pass.Password;
+            }
         }
     }
 }
--- LambovaT_TeaApp/SignUp.xaml.cs
+++ LambovaT_TeaApp/SignUp.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,9 +24,42 @@ namespace LambovaT_TeaApp
         }
         private void Confirm(object sender, RoutedEventArgs e)
         {
-            TeaCatalogue mwC = new TeaCatalogue();
-            mwC.Show();
-            this.Close();
+            if (First.Text.Equals("") || Last.Text.Equals(""))
+            {
+                MessageBox.Show("The fields 'First Name' and 'Last Name' shouldn't be left empty.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else if (!Email.Text.Contains("@"))
+            {
+                MessageBox.Show("The fields 'Email' needs to conatain '@'.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else if (!(PassRep.Password == Pass.Password))
+            {
+                MessageBox.Show("Passwords don't match.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                SqlConnection sqlCon = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=H:\2022_2023\Informatics\Applications\FinalTeaApp\LambovaT_TeaApp\LambovaT_TeaApp\TeaDB.mdf;Integrated Security=True");
+                try
+                {
+                    sqlCon.Open();
+                    string query = "INSERT INTO Users(FirstName,LastName,Email,Pass)values ('" + this.First.Text + "','" + this.Last.Text + "','" + this.Email.Text + "','" + this.Pass.Password + "') ";
+
+                    SqlCommand cmd = new SqlCommand(query, sqlCon);
+                    cmd.ExecuteNonQuery();
+
+                    MessageBox.Show("Account created");
+
+                    TeaCatalogue mwC = new TeaCatalogue();
+                    mwC.Show();
+                    this.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                
+            }
+            
         }
 
     }
